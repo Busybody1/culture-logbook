259e17cc1673563c529a89d8f88dabd9891574b2
unknown <busybody.office@gmail.com>
2025-03-13 16:24:56 +0000

    Implement travel map feature

---
+++ src/lib/fixLeafletIcon.ts
@@ -0,0 +1,13 @@
+import L from 'leaflet';
+
+// Fix the missing icon issue in Leaflet
+// This is needed because Leaflet expects the marker icons to be in specific locations
+export function fixLeafletIcon() {
+  delete (L.Icon.Default.prototype as any)._getIconUrl;
+  
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+  });
+}
